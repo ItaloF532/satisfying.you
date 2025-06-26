4302d224cd3fbf5f62d381c02c4172ae5a9e1266
ItaloF532 <italo.francisco532@gmail.com>
2025-06-26 19:30:49 -0300

    chore: some config

---
+++ src/services/firebaseConfig.ts
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { ENV } from "../const/Environments";
+
+const firebaseConfig = {
+  appId: "1:495706170404:android:a67ae3b6faa4837eefe4b1",
+  apiKey: "AIzaSyDH55-hxF7vSqsAEVqLQ57yRWJUa3AKk6M",
+  projectId: "dispositivos-moveis-edf9e",
+  authDomain: "dispositivos-moveis-edf9e.firebaseapp.com",
+  databaseURL: "https://dispositivos-moveis-edf9e.firebaseio.com",
+  storageBucket: `dispositivos-moveis-edf9e.firebasestorage.app`,
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export { auth };
